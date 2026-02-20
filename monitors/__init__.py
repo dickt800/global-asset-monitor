b68d54e63fa535f8f8d4b7166109236d6b19e2fc
zhanghaoyang <zhanghaoyang2002@gmail.com>
2026-02-20 12:00:58 -0500

    fix circular import

--- monitors/__init__.py
+++ monitors/__init__.py
@@ -1,14 +1,2 @@
-# Monitors package
 from .base_monitor import BaseMonitor
-from .fx_monitor import FXMonitor
-from .jd_monitor import JDMonitor
-from .amazon_monitor import AmazonMonitor
-from .flight_monitor import FlightMonitor
-
-__all__ = [
-    'BaseMonitor',
-    'FXMonitor',
-    'JDMonitor',
-    'AmazonMonitor',
-    'FlightMonitor'
-]
+from .fx_monitor_cn import FXMonitorCN
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,3 @@
-# Utils package
-from .anti_crawler import AntiCrawler
 from .notifier import BrevoNotifier
 from .persistence import PersistenceManager
-from .global_strategy import GlobalStrategy
-
-__all__ = [
-    'AntiCrawler',
-    'BrevoNotifier',
-    'PersistenceManager',
-    'GlobalStrategy'
-]
+from .anti_crawler import AntiCrawler
